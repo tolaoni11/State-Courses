551a6ee77a8e47ba2a640fb2df599087e8d8493e
tolaoni11 <tolaoni11@yahoo.com>
2023-11-28 12:40:20 -0600

    Color Changer Example

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import "./App.css";
 import FormExample from "./components/FormExample";
 import SliderExample from "./components/SliderExample";
+import ColorChanger from "./components/ColorChanger";
 
 function App() {
   return (
     <>
-      <SliderExample />
+      <ColorChanger />
     </>
   );
 }
--- src/components/ColorChanger.jsx
+++ src/components/ColorChanger.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function ColorChanger() {
+  const [color, setColor] = useState("lightgreen");
   return (
     <div>
-      <h2>ColorChanger</h2>
+      <h2>Color Changer</h2>
+      <div
+        style={{
+          width: "200px",
+          height: "200px",
+          background: color,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        Click Me!
+      </div>
     </div>
   );
 }
