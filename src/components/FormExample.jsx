c4eb2490b4e03ba002d214e2496d4d03aa5370c4
tolaoni11 <tolaoni11@yahoo.com>
2023-12-04 14:56:25 -0600

    Event Handler: onChange

--- src/components/FormExample.jsx
+++ src/components/FormExample.jsx
@@ -1,28 +1,45 @@
 import React, { useState } from "react";
 
 export default function FormExample() {
-  const [firstname, setFirstname] = useState("Tola");
-  const [lastname, setLastname] = useState("Oni");
-  const [email, setEmail] = useState("yahoo");
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+
+  console.log(firstname, lastname, email);
 
   return (
     <form>
       <p>
         <label>
           First Name:
-          <input type="text" name="firstname" value={firstname} />
+          <input
+            type="text"
+            name="firstname"
+            value={firstname}
+            onChange={(e) => setFirstname(e.target.value)}
+          />
         </label>
       </p>
       <p>
         <label>
           Last Name:
-          <input type="text" name="lastname" value={lastname} />
+          <input
+            type="text"
+            name="lastname"
+            value={lastname}
+            onChange={(e) => setLastname(e.target.value)}
+          />
         </label>
       </p>
       <p>
         <label>
           Email:
-          <input type="text" name="email" value={email} />
+          <input
+            type="text"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </label>
       </p>
     </form>
